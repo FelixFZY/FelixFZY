ebcdb6b999ace100b48dc714b7da7c41f6c3e815
Felix <98073177+FelixFZY@users.noreply.github.com>
2022-02-25 22:55:38 +0800

    Create config.php

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+// 网站标题
+const title = "荆棘留言板";
+// 数据库地址
+const db_host = "233.233.233.233";
+// 数据库名
+const db_name = "name";
+// 数据库用户名
+const db_username = "username";
+// 数据库密码
+const db_password = "password";
+// 数据库端口
+const db_port = 3306;
